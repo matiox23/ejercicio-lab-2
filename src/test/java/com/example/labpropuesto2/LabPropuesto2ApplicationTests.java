92ed614cbd2d956fa977d3b79cda88ecbeb18937
matiox23 <matias_ramirez@hotmail.com>
2023-04-23 19:15:38 -0500

    first commit

---
+++ src/test/java/com/example/labpropuesto2/LabPropuesto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labpropuesto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPropuesto2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
