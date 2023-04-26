48114f4acb5efe87684ec71a2f7d8304d0bc755f
matiox23 <matias_ramirez@hotmail.com>
2023-04-25 20:01:29 -0500

    Creando la clase BookController

--- src/main/java/com/example/labpropuesto2/controllers/BookController.java
+++ src/main/java/com/example/labpropuesto2/controllers/BookController.java
@@ -1,4 +1,13 @@
 package com.example.labpropuesto2.controllers;
 
+import com.example.labpropuesto2.models.Book;
+import com.example.labpropuesto2.services.BookService;
+
 public class BookController {
+
+    public BookService  bookService;
+    public void addBook(){
+        Book book = new Book();
+        bookService.addBook(book);
+    }
 }
