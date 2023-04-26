afc83ef93f0f27d9d797260c60418a7bb4cc3f5d
matiox23 <matias_ramirez@hotmail.com>
2023-04-25 20:01:49 -0500

    Creando la clase BookService

--- src/main/java/com/example/labpropuesto2/services/BookService.java
+++ src/main/java/com/example/labpropuesto2/services/BookService.java
@@ -1,4 +1,14 @@
 package com.example.labpropuesto2.services;
 
+import com.example.labpropuesto2.models.Book;
+import com.example.labpropuesto2.repositories.BookRepository;
+
 public class BookService {
+
+    public BookRepository bookRepository;
+
+    public void addBook(Book book) {
+    bookRepository.save(book);
+    }
+
 }
