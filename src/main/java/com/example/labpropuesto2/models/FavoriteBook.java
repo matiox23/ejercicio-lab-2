502238513fc26705a52837a635d36f5bd3ab01a2
matiox23 <matias_ramirez@hotmail.com>
2023-04-26 20:22:43 -0500

    Creando la Clase FavoriteBook con sus atributos

---
+++ src/main/java/com/example/labpropuesto2/models/FavoriteBook.java
@@ -0,0 +1,36 @@
+package com.example.labpropuesto2.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "favorite_books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteBook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
