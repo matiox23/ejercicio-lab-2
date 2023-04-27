b0c04360fe6836938ec01c6b44ab9ae3ad32bddb
matiox23 <matias_ramirez@hotmail.com>
2023-04-26 20:13:27 -0500

    Creando la Clase Review con sus atributos

---
+++ src/main/java/com/example/labpropuesto2/models/Review.java
@@ -0,0 +1,47 @@
+package com.example.labpropuesto2.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+
+    @Column(name = "created_at")
+    private LocalDateTime createAt;
+
+    @Column(name = "update_at")
+    private LocalDateTime upadateAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
