36cb8c429e2458a1f2d16fcd11b775de107624c9
matiox23 <matias_ramirez@hotmail.com>
2023-04-26 19:51:21 -0500

    Agregando los atributos a la clase Book

--- src/main/java/com/example/labpropuesto2/models/Book.java
+++ src/main/java/com/example/labpropuesto2/models/Book.java
@@ -1,4 +1,43 @@
 package com.example.labpropuesto2.models;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "image_url")
+    private String imageUrl ;
+
+    @Column(name = "created_at")
+    private LocalDateTime createAt;
+
+    @Column(name = "update_at")
+    private LocalDateTime upadateAt;
+
+
+
 }
