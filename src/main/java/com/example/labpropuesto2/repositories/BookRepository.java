a85cb76a4fe244b8cfd6cb58f8603fb9976b3221
matiox23 <matias_ramirez@hotmail.com>
2023-04-26 20:46:33 -0500

    Creando la Clase FavoriteBook con sus métodos

--- src/main/java/com/example/labpropuesto2/repositories/BookRepository.java
+++ src/main/java/com/example/labpropuesto2/repositories/BookRepository.java
@@ -1,8 +1,24 @@
 package com.example.labpropuesto2.repositories;
 
 import com.example.labpropuesto2.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class BookRepository {
-    public void save(Book book) {
-    }
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAll();
+    Optional<Book> findById(Long id);
+    List<Book> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String title, String author);
+    Book save( Book book);
+
+
+
+
+
+
+
+
+
 }
