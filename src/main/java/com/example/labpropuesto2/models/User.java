d7abdf5b1ac785a9062aeff9a9a5de26cbedc821
matiox23 <matias_ramirez@hotmail.com>
2023-04-26 19:57:07 -0500

    Creando la Clase User con sus atributos

---
+++ src/main/java/com/example/labpropuesto2/models/User.java
@@ -0,0 +1,31 @@
+package com.example.labpropuesto2.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
