9aa559babbb57f531a37f22d041685224d5636b4
matiox23 <matias_ramirez@hotmail.com>
2023-04-28 16:06:02 -0500

    Creando la clase BookController

--- src/main/java/com/example/labpropuesto2/controllers/BookController.java
+++ src/main/java/com/example/labpropuesto2/controllers/BookController.java
@@ -1,13 +1,45 @@
 package com.example.labpropuesto2.controllers;
-
 import com.example.labpropuesto2.models.Book;
 import com.example.labpropuesto2.services.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-public class BookController {
+import java.util.List;
 
-    public BookService  bookService;
-    public void addBook(){
-        Book book = new Book();
-        bookService.addBook(book);
+
+@RestController
+public class BookController  {
+    private final BookService bookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
+
+    @PostMapping
+    public ResponseEntity<Book>addBook(@RequestParam String title,
+                                        @RequestParam String author, @RequestParam String description,
+                                        @RequestParam MultipartFile image) {
+        Book newBook = bookService.addBook(title, author, description, image);
+        return ResponseEntity.ok(newBook);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.getAllBooks();
+        return ResponseEntity.ok(books);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Book book = bookService.getBookById(id);
+        return ResponseEntity.ok(book);
+    }
+
+
+
+
+
+
+
 }
