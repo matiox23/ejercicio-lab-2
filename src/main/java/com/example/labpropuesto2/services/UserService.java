486e4a246befed09143fe70832976e6b89f8358b
matiox23 <matias_ramirez@hotmail.com>
2023-04-28 16:03:33 -0500

    Modificando la clase UserService

--- src/main/java/com/example/labpropuesto2/services/UserService.java
+++ src/main/java/com/example/labpropuesto2/services/UserService.java
@@ -1,11 +1,24 @@
 package com.example.labpropuesto2.services;
 
+import com.example.labpropuesto2.models.Book;
 import com.example.labpropuesto2.models.User;
 
+import java.util.List;
+
 public interface  UserService {
+
 
     User createUser(User user);
     User getUserById(Long id);
     User getUserByUsername(String username);
 
+    User updateUser(Long id, User userDetails);
+
+    List<User> getAllUser();
+
+    void deleUser(Long id);
+
+    User register(String username, String password, String email);
+
+    User login(String username, String password);
 }
