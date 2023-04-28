88dd2a1a4e553c18b98412aa5138dd6935aeef88
matiox23 <matias_ramirez@hotmail.com>
2023-04-28 16:04:23 -0500

    Modificando la Interfaz FavoriteBookService

--- src/main/java/com/example/labpropuesto2/services/FavoriteBookService.java
+++ src/main/java/com/example/labpropuesto2/services/FavoriteBookService.java
@@ -2,11 +2,17 @@ package com.example.labpropuesto2.services;
 
 import com.example.labpropuesto2.models.Book;
 import com.example.labpropuesto2.models.FavoriteBook;
+import com.example.labpropuesto2.models.User;
 
 import java.util.List;
 
 public interface FavoriteBookService {
+    FavoriteBook saveFavorite(FavoriteBook favoriteBook);
+
+    void deleteFavoriteById(Long id);
+    List<FavoriteBook> getAllFavoritesByUser(User user);
+
     FavoriteBook addFavoriteBook(Long bookId, Long userId);
-    List <Book> getFavoriteBookByUserId( Long userId);
-    void removeFavoriteBook(Long bookId, Long userId);
+
+    List<Book> getFavoriteBooksByUserId(Long userId);
 }
